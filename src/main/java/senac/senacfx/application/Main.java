a35d6a60f7e747de89a8bfb35411c7771d951f96
matheusdfer <matheus.brother@hotmail.com>
2022-09-26 20:36:31 -0300

    twsfgcvgf

--- src/main/java/senac/senacfx/application/Main.java
+++ src/main/java/senac/senacfx/application/Main.java
@@ -18,6 +18,9 @@ public class Main extends Application {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
             ScrollPane scrollPane = loader.load();
 
+
+            
+
             //macete para a barra de cima ficar ate o fim da tela
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
