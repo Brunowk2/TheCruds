f3932cb2f260d47c281216f05bf44e21e71ed5f0
matheusdfer <matheus.brother@hotmail.com>
2022-10-03 19:22:50 -0300

    testando tabela

---
+++ src/main/java/senac/senacfx/model/dao/VendedorDao.java
@@ -0,0 +1,22 @@
+package senac.senacfx.model.dao;
+
+import senac.senacfx.model.entities.Department;
+import senac.senacfx.model.entities.Seller;
+import senac.senacfx.model.entities.Vendedor;
+
+import java.util.List;
+
+public interface VendedorDao {
+
+    void insert(Vendedor obj);
+    void update(Vendedor obj);
+
+
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+    List<Seller> findByDepartment(Department department);
+
+
+
+}
